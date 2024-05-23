5edc943394c72a0afb61c7bee3b5ec2aad2dc8ac
Peter NDENGO <frdrcpeter@gmail.com>
2024-05-23 08:33:40 +0100

    Serialization and comment code

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,24 +1,53 @@
 from django.conf import settings
 from rest_framework import serializers
 
-from .models import Category, Server
+from .models import Category, Channel, Server
 
 
+# Serializer pour le modèle Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
-        fields = ['id', 'name']
+        model = Category  # Modèle à sérialiser
+        fields = ['id', 'name']  # Champs à inclure dans la sérialisation
 
+# Serializer pour le modèle Channel
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel  # Modèle à sérialiser
+        fields = '__all__'  # Inclure tous les champs du modèle
 
-# class OwnerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = settings.AUTH_USER_MODEL
-#         fields = ['id', 'username']
+# Serializer pour le modèle User (utilisateur)
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = settings.AUTH_USER_MODEL  # Modèle utilisateur à partir des paramètres
+        fields = ['id', 'username']  # Champs à inclure dans la sérialisation
 
+# Serializer pour le modèle Server
 class ServerSerializer(serializers.ModelSerializer):
+    # Champ personnalisé pour le nombre de membres
+    num_members = serializers.SerializerMethodField()
+    # Sérialisation imbriquée pour la catégorie
     category = CategorySerializer(read_only=True)
-    # owner = OwnerSerializer(read_only=True)
+    # Sérialisation imbriquée pour les channels (nombreux)
+    channel_server = ChannelSerializer(read_only=True, many=True)
 
     class Meta:
-        model = Server
-        fields = '__all__'
+        model = Server  # Modèle à sérialiser
+        exclude = ("member",)  # Exclure le champ "member" de la sérialisation
+
+    # Méthode pour obtenir le nombre de membres
+    def get_num_members(self, obj):
+        if hasattr(obj, 'num_members'):
+            return obj.num_members
+        return None
+
+    # Personnalisation de la représentation des données
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get('num_members')
+
+        # Supprimer le champ 'num_members' si non nécessaire
+        if not num_members:
+            data.pop('num_members')
+        
+        return data
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -6,24 +8,50 @@ from .serializer import ServerSerializer
 
 
 class ServerListViewSet(viewsets.ViewSet):
-
+    # Définition de la queryset initiale qui récupère tous les objets Server
     queryset = Server.objects.all()
 
     def list(self, request):
+        # Récupération des paramètres de la requête
         category = request.query_params.get('category')
         limit = request.query_params.get('limit')
         by_user = request.query_params.get('by_user') == "true"
+        server_id = request.query_params.get('server')
+        with_num_members = request.query_params.get('with_num_members') == "true"
 
+        # Vérification de l'authentification si 'by_user' ou 'server_id' est spécifié
+        if (by_user or server_id) and not request.user.is_authenticated:
+            raise AuthenticationFailed(detail='Incorrect authentication credentials')
+
+        # Filtrer par 'server_id' si spécifié
+        if server_id:
+            try:
+                self.queryset = self.queryset.filter(id=server_id)
+
+                # Vérification si le serveur existe
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f'The server with id {server_id} not found')
+            except ValueError:
+                # Gestion des erreurs de conversion de 'server_id'
+                raise ValidationError(detail='Something went wrong !')
+
+        # Filtrer par 'category' si spécifié
         if category:
             self.queryset = self.queryset.filter(category=category)
         
+        # Filtrer par utilisateur si 'by_user' est spécifié
         if by_user:
-            print(request.user)
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+        
+        # Annoter le nombre de membres si 'with_num_members' est spécifié
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
+        # Limiter les résultats si 'limit' est spécifié
         if limit:
             self.queryset = self.queryset[:int(limit)]
         
-        serializer = ServerSerializer(self.queryset, many=True) 
+        # Sérialiser la queryset avec les données du serveur
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
