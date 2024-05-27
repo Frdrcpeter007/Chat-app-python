25047bd6e9df29a0ec8a93e937972c6bfdf77385
Peter NDENGO <frdrcpeter@gmail.com>
2024-05-27 11:16:19 +0100

    Manage banner and icon for the server

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
+from .validators import (validate_icon_image_size,
+                         validate_image_file_extensions)
+
+
+def server_banner_upload_path(instance, filename):
+    print(instance.id)
+    return f'server/{instance.id}/server_banner/{filename}'
+
+def server_icon_upload_path(instance, filename):
+    print(instance.id)
+    return f'server/{instance.id}/server_icon/{filename}'
 
 def category_icon_upload_path(instance, filename):
     print(instance.id)
@@ -39,6 +50,31 @@ class Server(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    banner = models.ImageField(upload_to=server_banner_upload_path, blank=True, null=True, validators=[validate_image_file_extensions],)
+    icon = models.ImageField(upload_to=server_icon_upload_path, blank=True, null=True, validators=[validate_icon_image_size, validate_image_file_extensions])
+
+   
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+
+        if self.id:
+            existing = get_object_or_404(Server, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        
+        super(Server, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender='server.Server')
+    def channel_delete_files(sender, instance,  **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
+
 
     def __str__(self) -> str:
         return self.name
---
+++ djchat/server/validators.py
@@ -0,0 +1,18 @@
+import os
+
+from django.core.exceptions import ValidationError
+from PIL import Image
+
+
+def validate_icon_image_size(image):
+    if image:
+        with Image.open(image) as img:
+            if img.width > 70 or img.height > 70:
+                raise ValidationError(f'Maximum allowed size for the image are 70x70 - size of image you uploaded: {img.size}')
+            
+def validate_image_file_extensions(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(f'That extension is not allowed to be uploaded - Ext: {ext}')
